0af87d8bc6124e006479e53de5f57e35ef43072a
rthomas98 <robt84@gmail.com>
2014-12-31 13:18:50 -0600

    content and new pages were added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
 
+if ($(window).scrollTop()===0){
+		$('#main-nav').removeClass('scrolled');
+	}
+	else{
+		$('#main-nav').addClass('scrolled');    
+	}
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop()===0){
+			$('#main-nav').removeClass('scrolled');
+		}
+		else{
+			$('#main-nav').addClass('scrolled');    
+		}
+	});
+    
+});
